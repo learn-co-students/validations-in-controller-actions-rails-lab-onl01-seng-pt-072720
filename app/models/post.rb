96ee43477786ffc56e28d521a9e4e55fe5a73ac2
agilles1 <adam.g.tpt@gmail.com>
2020-12-07 20:49:30 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    # The title cannot be blank
+    # The category is either "Fiction" or "Non-Fiction"
+    # The content is at least 100 characters long.
+    validates :title, presence: true
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validates :content, length: { minimum: 100 }
 end
